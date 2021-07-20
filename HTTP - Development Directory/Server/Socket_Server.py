34547e7d0d18463c4e6d609edf9b860fd69be4a4
ericatol <atolbirdmaker@gmail.com>
2021-07-20 00:18:14 -0700

    Added a 404 file not found reroute

--- HTTP - Development Directory/Server/Socket_Server.py
+++ HTTP - Development Directory/Server/Socket_Server.py
@@ -1,13 +1,15 @@
 import socket as SERVER
 
-HTTP_OK_200 = 'HTTP/1.0 200 OK\n\n'
-DOCUMENTS_HTML = '../Documents/HTML/'
+HTTP_OK_200 = "HTTP/1.0 200 OK\n\n"
+HTTP_NOT_FOUND_404 = "HTTP/1.0 404 NOT FOUND\n\n"
+DOCUMENTS_HTML = "../Documents/HTML/"
+HTML_404_LABEL = "404-File-Does-Not-Exist.html"
 
-DEBUGGING = True
+DEBUGGING = False
 
 # Define socket host and port
 
-SERVER_HOST = '0.0.0.0'
+SERVER_HOST = "0.0.0.0"
 SERVER_PORT = 8000
 
 # Define a server structure
@@ -24,7 +26,7 @@ class HTML_Server:
         print(f"This directory was read: {directory}\n" + "=_/\n")
         return self
 
-# Instantiate internet protocol as IPV4/TCP-IP
+# Instantiate the internet protocol to use as IPV4/TCP-IP
 
 SERVER_socket = SERVER.socket(SERVER.AF_INET, SERVER.SOCK_STREAM)
 SERVER_socket.setsockopt(SERVER.SOL_SOCKET, SERVER.SO_REUSEADDR, 1)
@@ -38,7 +40,8 @@ while True:
 
     client_Connection, client_Address = SERVER_socket.accept()
 
-    # When a new request is made at this IP, this data exists
+    # When a new request is made at this IP, this data exists:
+
     client_Request = client_Connection.recv(1024).decode()
     print(client_Request)
     REQUESTED_DIRECTORY_HEADERS = client_Request.split('\n')
@@ -46,12 +49,26 @@ while True:
     if REQUESTED_HTML_LABEL == '/' or REQUESTED_HTML_LABEL == "0x5C":
         REQUESTED_HTML_LABEL = "Interface.html"
 
-    accessPoint_HTML = HTML_Server()
+    try:
 
-    # Server opens and stores the default HTML page (useful for validating)
-    accessPoint_HTML = accessPoint_HTML.Response_GET(HTTP_OK_200).Directory_GET(DOCUMENTS_HTML + REQUESTED_HTML_LABEL)
+        # The server opens and temporarily stores the requested HTML page for routing
+
+        accessPoint_HTML = HTML_Server()
+        accessPoint_HTML = accessPoint_HTML.Response_GET(HTTP_OK_200).Directory_GET(DOCUMENTS_HTML + REQUESTED_HTML_LABEL)
+    except:
+
+        # If the requested page does not exist, or there is an error, the server opens and temporarily stores the 404 error HTML page for routing
+
+        accessPoint_HTML = HTML_Server()
+        accessPoint_HTML = accessPoint_HTML.Response_GET(HTTP_NOT_FOUND_404).Directory_GET(DOCUMENTS_HTML + HTML_404_LABEL)
+
+        # The data is then sent to the client in a byte stream
 
     client_Connection.sendall(accessPoint_HTML.Response.encode())
+
+        # After sending information, the endpoint is closed, and the connection is terminated
+
+    del accessPoint_HTML
     client_Connection.close()
     
     if DEBUGGING:
