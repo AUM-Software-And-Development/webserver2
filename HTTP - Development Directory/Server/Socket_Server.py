d9d3323326edf6770aa4f90b2dd4b9cb0c3eac4b
ericatol <atolbirdmaker@gmail.com>
2021-07-19 23:35:28 -0700

    Directory searching functionality

--- HTTP - Development Directory/Server/Socket_Server.py
+++ HTTP - Development Directory/Server/Socket_Server.py
@@ -1,7 +1,7 @@
 import socket as SERVER
 
 HTTP_OK_200 = 'HTTP/1.0 200 OK\n\n'
-HTML_INTERFACE = '../Documents/HTML/Interface.html'
+DOCUMENTS_HTML = '../Documents/HTML/'
 
 DEBUGGING = True
 
@@ -9,6 +9,20 @@ DEBUGGING = True
 
 SERVER_HOST = '0.0.0.0'
 SERVER_PORT = 8000
+
+# Define a server structure
+
+class HTML_Server:
+    def __init__(self):
+        self.Response = ''
+    def Response_GET(self, response):
+        self.Response += response
+        print("\033[1A" + "===\n" + self.Response + "GET Request received!\n" + "=\_")
+        return self
+    def Directory_GET(self, directory):
+        self.Response += open(directory).read() # add .close()
+        print(f"This directory was read: {directory}\n" + "=_/\n")
+        return self
 
 # Instantiate internet protocol as IPV4/TCP-IP
 
@@ -18,30 +32,24 @@ SERVER_socket.bind((SERVER_HOST, SERVER_PORT))
 SERVER_socket.listen(1)
 print("\n" + "Listening on port %s " % SERVER_PORT + "\n" + "________________________" "\n")
 
+# Open the server to requests
+
 while True:
 
     client_Connection, client_Address = SERVER_socket.accept()
 
     # When a new request is made at this IP, this data exists
-    request = client_Connection.recv(1024).decode()
-    print(request)
-
-    class HTML_Server:
-        def __init__(self):
-            self.Response = ''
-        def Response_GET(self, response):
-            self.Response += response
-            print("\033[1A" + "===\n" + self.Response + "GET Request received!\n" + "=\_")
-            return self
-        def Directory_GET(self, directory):
-            self.Response += open(directory).read()
-            print(f"This directory was read: {directory}\n" + "=_/\n")
-            return self
+    client_Request = client_Connection.recv(1024).decode()
+    print(client_Request)
+    REQUESTED_DIRECTORY_HEADERS = client_Request.split('\n')
+    REQUESTED_HTML_LABEL = REQUESTED_DIRECTORY_HEADERS[0].split()[1]
+    if REQUESTED_HTML_LABEL == '/' or REQUESTED_HTML_LABEL == "0x5C":
+        REQUESTED_HTML_LABEL = "Interface.html"
 
     accessPoint_HTML = HTML_Server()
 
     # Server opens and stores the default HTML page (useful for validating)
-    accessPoint_HTML = accessPoint_HTML.Response_GET(HTTP_OK_200).Directory_GET(HTML_INTERFACE)
+    accessPoint_HTML = accessPoint_HTML.Response_GET(HTTP_OK_200).Directory_GET(DOCUMENTS_HTML + REQUESTED_HTML_LABEL)
 
     client_Connection.sendall(accessPoint_HTML.Response.encode())
     client_Connection.close()
